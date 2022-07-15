88aa622d04a9cab883cf1a9ff8f8e99a73272118
Michal <mpiatek0611@gmail.com>
2022-07-15 21:47:15 +0200

    cz1 lab4

---
+++ skrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+case $1 in --help) echo "wypisz:
+--help aby wyswietlic pomoc
+--date aby wyswietlic date
+--logs aby stworzyc 100 plikow lognumer.txt
+--logs >liczba< aby stworzyc wybrana ilosc lognumer.txt plikow";;
+done;;
+esac
