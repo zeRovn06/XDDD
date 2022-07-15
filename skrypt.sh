7b5108cd6f75b8b473405c0fb8eff216c6edd673
zeRovn06 <109021634+zeRovn06@users.noreply.github.com>
2022-07-15 22:15:03 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-case $1 in --help) echo "wypisz:
+case $1 in
+--help)
+echo
+"Wpisz:
 --help aby wyswietlic pomoc
---date aby wyswietlic date
---logs aby stworzyc 100 plikow lognumer.txt
---logs >liczba< aby stworzyc wybrana ilosc lognumer.txt plikow";;
-done;;
+--date aby wyswietlic godzine
+--logs aby stworzyc 100 plikow log<numer>.txt
+--logs <liczba> aby stworzyc wybrana przez siebie liczbe plikow log<liczba>.txt
+"
+;;
+--date)
+echo `date +'%m/%d/%Y'`
+;;
+--logs)
+if [ -z "$2" ]
+then
+pliki=100
+else
+pliki=$2
+fi
+for i in $( eval echo {1..$pliki} )
+do
+echo log$i.txt $0 `date +'%m/%d/%Y'` >> log$i.txt
+done
+;;
 esac
